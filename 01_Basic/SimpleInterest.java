91b89774ea5ae8a961fd0b31e4c64373add8ab0d
Sujan Samanta <89310577+sujansamanta@users.noreply.github.com>
2025-06-23 18:06:23 +0530

    Add files via upload

---
+++ 01_Basic/SimpleInterest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principle(Amount): ");
+        float p = sc.nextFloat();
+
+        System.out.print("Enter rate of interest: ");
+        float r = sc.nextFloat();
+
+        System.out.print("Enter time(Year): ");
+        float t = sc.nextFloat();
+
+
+        float si = (p*r*t)/100;
+        
+        System.out.println("Total Simple Interest is:"+ si);
+    }
+    
+}
