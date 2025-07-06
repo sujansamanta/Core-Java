b39dc06844bf24f1062edf5603394f77fbd8c5b9
sujansamanta <sujansamanta21@gmail.com>
2025-07-06 22:29:15 +0530

    Basic functions of operator

---
+++ 01_Basic/ReadChar.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class ReadChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Charecter:");
+        char ch = sc.next().charAt(0);
+        System.out.println("Your charecter is:"+ch);
+    }
+    
+}
---
+++ 01_Basic/input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Your name is :" + name);
+
+        System.out.print("Enter your lucky number:");
+        int num1 = sc.nextInt();
+        System.out.println("Lucky number is:" + num1);
+    }
+}
